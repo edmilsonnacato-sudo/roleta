49c25dccff9e4b624ac9730c2b8a6f7509a103d6
edmilsonnacato-sudo <edmilsonnacato@gmail.com>
2026-02-14 23:23:21 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { obfuscator } from 'vite-plugin-obfuscator';
+import obfuscator from 'vite-plugin-javascript-obfuscator';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
